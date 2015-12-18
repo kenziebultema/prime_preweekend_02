195e126eaf76ad8b05c54cbcdd9de8a88ebcfc25
Scott Bromander <scott@primeacademy.io>
2015-12-18 13:50:47 -0600

    update

---
+++ server/public/scripts/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    console.log("Meow! (Really would you have expected something else from me?");
+
+    $(".clicky-button").on('click', clickyButton);
+
+
+});
+
+function clickyButton(){
+    var data = {};
+
+    $.ajax({
+        type: "GET",
+        url: "/data",
+        success: function(gremlins){
+            data = gremlins;
+            logData(data);
+        }
+    });
+
+
+}
+
+function logData(data){
+    console.log(data);
+}
+
